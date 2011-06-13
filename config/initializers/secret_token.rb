689da2e8decd19331910604181037c9ddded0807
Tien H. Nguyen <nht007@gmail.com>
2011-06-13 10:17:24 -0500

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TictactienRails::Application.config.secret_token = '8f9adeacf2c90f9eab678846c0baf99173e7b980e76a87ccf5f66a07640ae9072d2e5ab6560a818cb59ccee133083383edb3d9d605731427b94296428b00c0ef'
